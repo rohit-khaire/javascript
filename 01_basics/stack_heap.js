e0d74c63d1d8e7de3f8442d7c3f189b0c2a52805
rohit-khaire <athlius31@gmail.com>
2024-09-11 14:19:46 +0000

    heap and stack memory storage

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,11 @@
+let a=1
+let b =a
+a=5
+console.log(a)  //5
+console.log(b)  //1
+
+let userOne={email:"user@g.com", upi: "user@ybl"}
+let userTwo = userOne
+userTwo.email="rohit@g.com"
+console.log(userOne.email)
+console.log(userTwo.email)
