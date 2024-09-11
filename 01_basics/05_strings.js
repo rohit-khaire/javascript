d39645fe4194d2fe828bc54d32ea58a3bd0de595
rohit-khaire <athlius31@gmail.com>
2024-09-11 15:25:58 +0000

    String methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+let name = "Rohit"
+const age = 19
+name = new String('Khaire')
+console.log(name+" : "+age+" Age")
+
+console.log(`${name} : ${age} Age`);
+console.log(name[0]) //=> K
+console.log(name.toUpperCase()) //=> KHAIRE
+console.log(name.length)
+console.log(name.charAt(2))
+console.log(name.indexOf('r'))
+console.log(name.substring(0,3))
+console.log(name.split('i'))
+//console.log(name.slice(0,5))
+console.log(name.slice(-6,2))
+console.log("   RK   ".trimEnd())
+
+console.log("https://rohit%20khaire.com".replace('%20','-'))
+console.log("khairerohit".includes('Rohit'))
