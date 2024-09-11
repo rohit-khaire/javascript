7137c1fe1735b8ff81116cb70e104c3a44926622
rohit-khaire <athlius31@gmail.com>
2024-09-11 10:09:25 +0000

    Type casting

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let N_Str = "55"
+let AN_Str = "23abc"
+let nl = null
+let undef = undefined
+let cN_str= Number(N_Str)
+let cAN_Str = Number(AN_Str)
+let cnl = Number(nl)
+let cundef = Number(undef)
+console.log("Before Coversion")
+console.table([N_Str,AN_Str,nl,undef])
+console.log("After Coversion")
+console.table([cN_str,cAN_Str,cnl,cundef])
+console.log("Type of NaN is ")
+console.table([typeof cN_str,typeof cAN_Str,typeof cnl,typeof cundef])
