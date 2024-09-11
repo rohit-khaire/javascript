4e03897b28d7dbbe38ed99c72a9918ed3b256529
rohit-khaire <athlius31@gmail.com>
2024-09-11 18:03:17 +0000

    num and maths

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,18 @@
+// let score = 23
+// console.log(score)
+
+// let a = new Number(100)
+// console.log(a.toFixed(2))
+// a=123.8966
+// console.log(a.toPrecision(1))
+// console.log(a.toPrecision(2))
+// console.log(a.toPrecision(3))
+// console.log(a.toPrecision(4))
+// console.log(a.toPrecision(5))
+
+// console.log(Math.round(4.3)) //=> 4
+// console.log(Math.round(4.5)) //=> 5
+// console.log(Math.round(4.6)) //=> 5
+let min =10
+let max = 11
+console.log(Math.floor(Math.random()*(max-min))+min)
